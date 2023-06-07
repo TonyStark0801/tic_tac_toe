59c61972f6764e4a839825afdd46479d5cf01c89
Shubham Mishra <mishrashubham2001@gmail.com>
2023-06-07 22:55:58 +0530

    Checking winners

--- lib/screens/games.dart
+++ lib/screens/games.dart
@@ -65,5 +65,68 @@ class _GameScreenState extends State<GameScreen> {
         ));
   }
 
+  void _tapped(int index) {
+    setState(() {
+      if (oTurn == true && displayXO[index] == '') {
+        displayXO[index] = 'O';
+      } else if (oTurn == false && displayXO[index] == '') {
+        displayXO[index] = 'X';
+      }
+      oTurn = !oTurn;
+      tapCounter++;
+      if (tapCounter >= 5) checkWinner();
+    });
+  }
+
+  void checkWinner() {
+    //Row
+    int i = 0;
+    int interIndex = 0;
+    while (i < 3) {
+      rowCheck(interIndex);
+      interIndex += 3;
+      i++;
+    }
+    //Col
+    for (var i = 0; i < 3; i++) {
+      colCheck(i);
+    }
+    //Dialognal
+    diagonalCheck();
+  }
+
+  void rowCheck(int index) {
+    if (displayXO[index] == displayXO[index + 1] &&
+        displayXO[index] == displayXO[index + 2] &&
+        displayXO[index] != '') {
+      resultDeclare(index);
+    }
+  }
+
+  void colCheck(int index) {
+    if (displayXO[index] == displayXO[index + 3] &&
+        displayXO[index] == displayXO[index + 6] &&
+        displayXO[index] != '') {
+      resultDeclare(index);
+    }
+  }
+
+  void diagonalCheck() {
+    if (displayXO[0] == displayXO[4] &&
+        displayXO[0] == displayXO[8] &&
+        displayXO[0] != '') {
+      resultDeclare(0);
+    } else if (displayXO[2] == displayXO[4] &&
+        displayXO[2] == displayXO[6] &&
+        displayXO[2] != '') {
+      resultDeclare(2);
+    }
+  }
+
+  resultDeclare(int index) {
+    setState(() {
+      resultDeclaration = 'PLAYER ${displayXO[index]} WINS!!';
+    });
+  }
   
 }
